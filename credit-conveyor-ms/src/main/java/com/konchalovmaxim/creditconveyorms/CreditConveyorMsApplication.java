12e6b1ff22f20a00e1380093c645c4c16f2fe14c
SpiceMAX <88837535+SpiceMAX@users.noreply.github.com>
2022-05-20 17:17:02 +0300

    init

---
+++ credit-conveyor-ms/src/main/java/com/konchalovmaxim/creditconveyorms/CreditConveyorMsApplication.java
@@ -0,0 +1,13 @@
+package com.konchalovmaxim.creditconveyorms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditConveyorMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreditConveyorMsApplication.class, args);
+    }
+
+}
---
+++ credit-conveyor-ms/src/test/java/com/konchalovmaxim/creditconveyorms/CreditConveyorMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.konchalovmaxim.creditconveyorms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditConveyorMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
